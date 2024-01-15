2a4990a1f38afa890f351694e7d3ee01ad4e199b
achorn <joshuaachorn@gmail.com>
2024-01-15 10:40:42 -0800

    adding required libraries and setting up mongoose and express methods

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { default: mongoose } = require("mongoose");
+const bodyParser = require("body-parser");
+const { default: mongoose, mongo } = require("mongoose");
 require("dotenv").config();
-
-mongoose.connect(process.env.MONGO_URI);
 
 app.use(cors());
 app.use(express.static("public"));
 
+//MONGOOSE METHODS
+mongoose.connect(process.env.MONGO_URI);
+
+const userSchema = mongoose.Schema({
+  username: "String",
+  //_id is created automatically
+});
+const User = mongoose.model("User", userSchema);
+
+//EXPRESS METHODS
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/api/users", (req, res) => {
+  //get user name from body parser
+  // create new user
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
